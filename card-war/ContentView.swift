46f7c3d8054c67f33a8856f46620c80c9933641d
mateuszdylewski <mateuszdylewski@yahoo.com>
2022-08-02 23:44:31 +0200

    finished game

--- card-war/ContentView.swift
+++ card-war/ContentView.swift
@@ -8,9 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var playerCard = "back"
+    @State private var cpuCard = "back"
+    
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background")
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                
+                Button(action: {
+                    let playerRand = Int.random(in: 2...13)
+                    let cpuRand = Int.random(in: 2...13)
+                    
+                    playerCard = "card\(playerRand)"
+                    cpuCard = "card\(cpuRand)"
+                    
+                    if playerRand > cpuRand {
+                        playerScore += 1
+                    } else if playerRand < cpuRand {
+                        cpuScore += 1
+                    }
+                }, label: {
+                    Image("dealbutton")
+                })
+                
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 5.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 5.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .foregroundColor(Color.white)
+                Spacer()
+            }
+        }
     }
 }
 
